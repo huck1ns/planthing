7d21fc8f289c185b9b36c11ccaa5da5f19e31886
huckinsjatwit <jackdhuckins@gmail.com>
2026-01-28 12:08:13 -0500

    lots of improvements

--- Display_Indicator.py
+++ Display_Indicator.py
@@ -1,14 +1,14 @@
 from PySide6.QtSvgWidgets import QSvgWidget
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QStackedLayout, QSizePolicy
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QStackedLayout, QVBoxLayout, QLabel
 from PySide6.QtCore import Qt
 
-class Display_Indicator(QWidget):
+class Picture_Stack(QWidget):
     def __init__(self):
         super().__init__()
         
-        
-        self.layout = QStackedLayout(self)
-        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        #Pictures
+        self.pictures = QStackedLayout(self)
+        self.pictures.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
     
         self.livingSVG = QSvgWidget('resources/livingplant.svg')
@@ -18,26 +18,75 @@ class Display_Indicator(QWidget):
         for svg in (self.livingSVG, self.deadSVG, self.emptySVG):
             svg.renderer().setAspectRatioMode(Qt.KeepAspectRatio)
 
-        self.layout.addWidget(self.deadSVG)           # Dead in index 0
-        self.layout.addWidget(self.livingSVG)         # Living in index 1
-        self.layout.addWidget(self.emptySVG)          # Empty pot in index 2
+        self.pictures.addWidget(self.deadSVG)           # Dead in index 0
+        self.pictures.addWidget(self.livingSVG)         # Living in index 1
+        self.pictures.addWidget(self.emptySVG)          # Empty pot in index 2
         
         self.setToEmpty()
         
     def setToDead(self):
-        self.layout.setCurrentIndex(0)
+        self.pictures.setCurrentIndex(0)
     
     def setToLiving(self):
-        self.layout.setCurrentIndex(1)
+        self.pictures.setCurrentIndex(1)
     
     def setToEmpty(self):
-        self.layout.setCurrentIndex(2)
+        self.pictures.setCurrentIndex(2)
         
     def swapImages(self):
-        if self.layout.currentIndex() == 0:
-            self.setToLiving()
-        else: self.setToDead()
+        nextIndex = (self.pictures.currentIndex()+1)%3
+        self.pictures.setCurrentIndex(nextIndex)
+        
+class Value_Pane(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.textBox = QVBoxLayout(self)
+        self.textBox.setSpacing(0)
+        self.textBox.addStretch()
+        
+        self.waterLabel = QLabel()
+        self.waterExpectationLabel = QLabel()
+        self.waterUpdate("0", "0")
+        
+        self.lightLabel = QLabel()
+        self.lightExpectationLabel = QLabel()
+        self.lightUpdate("0", "0")
+        
+        self.textBox.addWidget(self.waterLabel)
+        self.textBox.addWidget(self.waterExpectationLabel)
+        self.textBox.addSpacing(20)
+        self.textBox.addWidget(self.lightLabel)
+        self.textBox.addWidget(self.lightExpectationLabel)
+        self.textBox.addStretch()
+        
+    def waterUpdate(self, waterLevel, expected):
+        WATER_TEXT = f"Current Water Level: {waterLevel}"
+        EXPECTATION = f"Expected Water Level: {expected}"
+        
+        self.waterLabel.setText(WATER_TEXT)
+        self.waterExpectationLabel.setText(EXPECTATION)
+        
+    def lightUpdate(self, lightLevel, expected):
+        LIGHT_TEXT = f"Current Light Level: {lightLevel}"
+        EXPECTATION = f"Expected Light Level: {expected}"
+        
+        self.lightLabel.setText(LIGHT_TEXT)
+        self.lightExpectationLabel.setText(EXPECTATION)
         
         
+class Display_Indicator(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.layout = QHBoxLayout(self)
+        
+        self.pictures = Picture_Stack()
+        self.values = Value_Pane()
+        
+        self.layout.addWidget(self.pictures, 1)
+        self.layout.addWidget(self.values, 1)
+        
+        self.setFixedWidth(400)
     
         
--- Holder.py
+++ Holder.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel
 from PySide6.QtCore import Qt
-from Display_Indicator import Display_Indicator
+from Display_Indicator import Picture_Stack, Display_Indicator, Value_Pane
 from PySide6.QtGui import QFont
 
 class Holder(QMainWindow):
@@ -8,8 +8,9 @@ class Holder(QMainWindow):
         super().__init__()
         self.setWindowTitle("Planthing!")
         self.resize(800, 600)
-        self.setMinimumSize(800,600)
-        self.setMaximumSize(800,600)
+        #self.setMinimumSize(800,600)
+        #self.setMaximumSize(800,600)
+        self.setStyleSheet("QMainWindow { background-color: #162c3d; }")
         
         vbox = QVBoxLayout()
         vbox.setSpacing(40)
@@ -21,19 +22,20 @@ class Holder(QMainWindow):
         font = QFont("Cascadia Mono", 20)
         font.setBold(True)
         self.plantNameLabel.setFont(font)
-        vbox.addWidget(self.plantNameLabel)
+        vbox.addWidget(self.plantNameLabel, alignment=Qt.AlignCenter)
+        
 
         
         self.imageDisplay = Display_Indicator()
         self.imageDisplay.setMaximumSize(200,500)
-        vbox.addWidget(self.imageDisplay)
+        vbox.addWidget(self.imageDisplay, alignment=Qt.AlignCenter)
         
-        vbox.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        #vbox.setAlignment(Qt.AlignmentFlag.AlignCenter) 
         vbox.addStretch()
         
         testImageButton = QPushButton("Press to change image")
-        testImageButton.clicked.connect(self.imageDisplay.swapImages)
-        vbox.addWidget(testImageButton)
+        testImageButton.clicked.connect(self.imageDisplay.pictures.swapImages)
+        vbox.addWidget(testImageButton, alignment=Qt.AlignCenter)
         
         layoutWidget = QWidget()
         
@@ -46,9 +48,3 @@ class Holder(QMainWindow):
     def setPlantName(self, name):
         combinedName = f"Plant name: {name}"
         self.plantNameLabel.setText(combinedName)
-        
-        
-        
-
-        
-        
